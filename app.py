dbb3a56d53e2032920c69ce5906ed3377fe6961a
Marps22 <157221429+Marps22@users.noreply.github.com>
2024-01-20 22:15:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import requests
 import streamlit as st
-from st.lottie import st_lottie
+from lottie import st_lottie
 
 st.set_page_config(page_title="Sweet Home Bakes", page_icon=":🍰:", layout="wide")
 
